e557c860f6e552c0d7ec7a96419ddcca29e6690f
vjyhh <ryabovoldenya@gmail.com>
2022-08-25 21:26:58 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import Counter from "./Components/Counter";
-
+import "./styles/App.css";
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <div className="post">
+        <div className="post_content">
+          <strong>1. Java Script</strong>
+          <div>Java Script - program lang</div>
+        </div>
+        <div className="post_btns">
+          <button>Удалить пост</button>
+        </div>
+      </div>
     </div>
   );
 }
